1e2a20c5115e4a8e83f8f35416341074cc9c1b70
snehareddy52 <reddysneha1503@gmail.com>
2025-03-12 13:22:08 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -4,7 +4,7 @@ from groq import Groq
 
 st.set_page_config(page_icon="💬", layout="wide",
                    page_title="PragyanAI Groq Streamlit APP")
-
+st.title("SNEHA REDDY's Chat Bot")
 st.divider()  # 👈 Draws a horizontal rule
 
 def icon(emoji: str):
